eb1aee32a7dab66f02c6eaeab5bd882d04ebc394
MJets09 <mthaoqk@gmail.com>
2023-02-20 18:54:59 -0600

    2.20.23

--- js.js
+++ js.js
@@ -2,8 +2,10 @@ let url = 'https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=Blue-Eyes'
 
 // https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=Blue-Eyes
 
-fetch(url)
+fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=Blue-Eyes')
+    //URL is stored with the API link
     .then(res => res.json())
+    //res is the data that is being sent back, could be named anything. Res is turned into json
     .then(data => {
         console.log(data)
         document.querySelector('img').src = data.data[6].card_images[0].image_url
